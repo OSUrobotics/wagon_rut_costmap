cbfc32318e81b75a6b9d4e60e42a0ab2207d8868
Hbeilinson <hcbeilinson@gmail.com>
2018-07-30 00:04:31 -0700

    Update efface_current_pose.cpp

--- src/efface_current_pose.cpp
+++ src/efface_current_pose.cpp
@@ -4,7 +4,8 @@
 #include <math.h>
 
 
-//This layer simply sets the cost in the robot's current location to be zero. This functions as a "wagon rut" costmap layer.
+//This layer  sets the cost in the robot's current location to be zero. This functions as a "wagon rut" costmap layer.
+//It also saves the current costmap to a file (alternating between map_a and map_b) so that it can be reloaded on the next launch by the load_old_map layer.
 PLUGINLIB_EXPORT_CLASS(wagon_rut_costmap_namespace::EffaceLayer, costmap_2d::Layer)
 
 using costmap_2d::NO_INFORMATION;
@@ -14,28 +15,7 @@ using costmap_2d::LETHAL_OBSTACLE;
 namespace wagon_rut_costmap_namespace
 {
 
-// double gaussian(double x, double y, double x0, double y0, double A, double varx, double vary, double skew){
-//     double dx = x-x0, dy = y-y0;
-//     double h = sqrt(dx*dx+dy*dy);
-//     double angle = atan2(dy,dx);
-//     double mx = cos(angle-skew) * h;
-//     double my = sin(angle-skew) * h;
-//     double f1 = pow(mx, 2.0)/(2.0 * varx),
-//            f2 = pow(my, 2.0)/(2.0 * vary);
-//     return A * exp(-(f1 + f2));
-// }
-
-double gaussian(double dist, double A, double varx, double vary, double mx, double my){
-    // double dx = x-x0, dy = y-y0;
-    double h = dist;
-    // double angle = angle;
-    // double mx = cos(angle-skew) * h;
-    // double my = sin(angle-skew) * h;
-    double f1 = pow(mx, 2.0)/(2.0 * varx),
-           f2 = pow(my, 2.0)/(2.0 * vary);
-    return A * exp(-(f1 + f2));
-}
-
+//This function determines whether the costmap will be saved as map_a or map_b, so that it alternates each time the costmap is launched
 //Update the location of the file to a stable location where you want to save data
 bool update_filename()
 {
@@ -48,31 +28,28 @@ bool update_filename()
   file.close();
   if (content == "true") {
     result = true;
-    // file.close();
     file.open("/home/strider/catkin_ws/src/wagon_rut_costmap/maps/filename.txt", ios::out | ios::trunc);
     file << "false";
     file.close();
   } else if (content == "false") {
     result = false;
-  //   // file.close();
     file.open("/home/strider/catkin_ws/src/wagon_rut_costmap/maps/filename.txt", ios::out | ios::trunc);
     file << "true";
     file.close();
   } else {
     return true;
   }
-  // file.close();
   return result;
-  // return true;
 }
 
 
 
-
+//This is the class containing the actual wagon rut costmap layer
 EffaceLayer::EffaceLayer() {}
 
 void EffaceLayer::onInitialize()
 {
+  //This code is necessary for all costmap layers
   ros::NodeHandle nh("~");
   current_ = true;
   default_value_ = NO_INFORMATION;
@@ -82,23 +59,22 @@ void EffaceLayer::onInitialize()
   dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(&EffaceLayer::reconfigureCB, this, _1, _2);
   dsrv_->setCallback(cb);
 
-  // bool map_a;
-  // nh.param("map_a", map_a, true);
+  //The following block of code sets up the system to save the costmap to alternating locations upon each launch
   map_a = update_filename();
-  // map_a = true;
   if (map_a) {
     file_name = "/home/strider/catkin_ws/src/wagon_rut_costmap/maps/map_a.pgm";
   } else {
     file_name = "/home/strider/catkin_ws/src/wagon_rut_costmap/maps/map_b.pgm";
   }
-  // ROS_INFO_STREAM(file_name);
-  moving = false;
 
+  //This variable determines whether the robot is moving, so that the robot will not create wagon ruts when it is still
+  moving = false;
+  //It then subscribes to the velocity topic
   movement_sub = nh.subscribe("/mobile_base/commands/velocity", 1, &EffaceLayer::movement_callback, this);
-  // nh.setParam("map_a", !map_a);
 
 }
 
+//Updates moving variable
 void EffaceLayer::movement_callback(const geometry_msgs::Twist& twist)
 {
   if ((twist.linear.x == 0.0) && (twist.linear.y == 0.0) && (twist.linear.z == 0.0)) {
@@ -108,22 +84,24 @@ void EffaceLayer::movement_callback(const geometry_msgs::Twist& twist)
   }
 }
 
-void EffaceLayer::matchSize() //This is a necessary function for every costmap layer
+//This is a necessary function for every costmap layer
+void EffaceLayer::matchSize() 
 {
   Costmap2D* master = layered_costmap_->getCostmap();
   resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
             master->getOriginX(), master->getOriginY());
 }
 
-
+//This is a necessary function for every costmap layer
 void EffaceLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
 {
   enabled_ = config.enabled;
 }
-
+    
+//This function sets the cost in this layer of the costmap to 20 at the current location of the robot, with a linearly decreasing cost around the robot's point.
+//It also changes the bounds being updated in the costmap to be the entire map.
 void EffaceLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
                                            double* min_y, double* max_x, double* max_y)
-//This function sets the cost in this layer of the costmap to zero at the current location of the robot
 {
   if (!enabled_)
     return;
@@ -137,10 +115,8 @@ void EffaceLayer::updateBounds(double robot_x, double robot_y, double robot_yaw,
   costmap_2d::Costmap2D* costmap = layered_costmap_->getCostmap();
 
   if(worldToMap(mark_x, mark_y, mx, my)){ //Checks that the location to change is a valid location on the map
-    // ROS_INFO_STREAM("updating");
     int cost = getCost(mx, my);
-    //ROS_INFO_STREAM(cost);
-    setCost(mx, my, 20); //Sets the value to zero in this location on the layer
+    setCost(mx, my, 20); //Sets the value to 20 in this location on the layer
 
 
     for (int i = 2; i < 5; i++) {
@@ -151,11 +127,7 @@ void EffaceLayer::updateBounds(double robot_x, double robot_y, double robot_yaw,
         if (t > 175 & t < 185) {
           setCost(x, y, 0);
         } else {
-          // int a = gaussian(i, 1.0, 2.0, 2.0, mx, my);
-        // ROS_INFO_STREAM(a);
-        // ROS_INFO_STREAM(a);
-          setCost(x, y, -4*i + 20);
-          // setCost(x, y, a);
+          setCost(x, y, -4*i + 20); //Sets up a decreasing cost radiating out from the robot starting from 20 at the point of the robot
         }
       }
     }
@@ -169,18 +141,13 @@ void EffaceLayer::updateBounds(double robot_x, double robot_y, double robot_yaw,
 
 }
 
+//This is where the change to the costs in the master map occurs
 void EffaceLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
                                           int max_j)
-//This is where the change to the costs in the master map occurs
 {
   if (!enabled_)
     return;
-  //
-  // if (!moving) {
-  //   master_grid.saveMap(file_name);
-  //   return;
-  // }
-  // ROS_INFO_STREAM("updating");
+
   for (int j = min_j; j < max_j; j++) //Iterates through the entire costmap
   {
     for (int i = min_i; i < max_i; i++)
@@ -197,17 +164,10 @@ void EffaceLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int
         continue;
       } else {
         // master_grid.setCost(i, j, 0);
-        master_grid.setCost(i, j, old_cost - costmap_[index]); //Sets the cost in the current location to 0
+        master_grid.setCost(i, j, old_cost - costmap_[index]); //Sets the cost in the current location to the previous cost in this location, minus the cost set for this layer in the updateBounds method
       }
     }
   }
-  // if (map_a) {
-  //   master_grid.saveMap("/home/strider/catkin_ws/src/simple_costmap_layer/world_files/map_a.pgm");
-  // } else {
-  //   master_grid.saveMap("/home/strider/catkin_ws/src/simple_costmap_layer/world_files/map_b.pgm");
-  // }
-  // moving = false;
-  // ROS_INFO_STREAM(file_name);
   master_grid.saveMap(file_name);
 }
 }
